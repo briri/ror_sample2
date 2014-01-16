cf49482a7d7ed46b198657fdb6d544f5bea79dd5
briley <riley.bri@gmail.com>
2014-01-16 18:20:47 +0000

    Created rails project and initial welcome controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '4ef69c5fe36a316e9274e89f3bf18a69bf3e16fa61d36d09b258168fc93d14507da1d315c182dd417c01fee4715569b8487f154bc2879fc6550e553563b3a364'
