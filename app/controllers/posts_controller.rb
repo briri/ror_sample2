3f74150bafd27802e8b81145aeac03fa58230baf
student <student@academyx.com>
2014-01-16 10:52:30 -0800

    adding posts controller and new.html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,10 +8,15 @@ class PostsController < ApplicationController
     # Examine the form post
     # render text: params[:post].inspect
 
-    @post = Post.new(params[:post])
+    @post = Post.new(post_params)
     @post.save
 
     redirect_to @post
+  end
+
+private
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 
 end
