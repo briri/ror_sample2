0f6afaffb9045990d8d6bf9ce5258b3269cfcc0d
student <student@academyx.com>
2014-01-17 11:07:09 -0800

    fixing issue with regex on user model for email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   validates :login, :presence => true, :uniqueness => true, :length => {minimum: 5}
   validates :password, :presence => true, :length => {minimum: 5}
   validates :email, :presence => true, :uniqueness => true,
-                    :format => {with: /[a-zA-Z0-9]+@[a-zA-Z0-9]+[a-zA-Z]{2,3}/}
+                    :format => {with: /[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,3}/}
 end
